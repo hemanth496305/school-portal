73e5c566659a15c060ce3ed7508debcf3959f496
raman <raman@example.com>
2025-10-23 17:37:00 +0530

    cleared

--- models/Subject.js
+++
@@ -1,9 +0,0 @@
-// models/Subject.js
-const mongoose = require('mongoose');
-
-const SubjectSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  teacher: { type: mongoose.Schema.Types.ObjectId, ref: 'User' } // employee user id
-});
-
-module.exports = mongoose.model('Subject', SubjectSchema);
