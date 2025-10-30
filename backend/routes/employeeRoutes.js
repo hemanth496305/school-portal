e20d8bed93750d9d924187992ff2db076994e335
root <root@webpage.nrtf2midc13u1ewebb3wiboura.rx.internal.cloudapp.net>
2025-10-30 10:15:46 +0000

    web pages added

--- backend/routes/employeeRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { getTimetable } from "../controllers/employeeController.js";
-
-const router = express.Router();
-router.get("/timetable", getTimetable);
-export default router;
-
--- backend/routes/studentRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getProfile, getTimetable } from "../controllers/studentController.js";
-
-const router = express.Router();
-router.get("/profile", getProfile);
-router.get("/timetable", getTimetable);
-export default router;
-
